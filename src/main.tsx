3500ef4deeb0ee602cbe120ce44aac8bc3ab4359
mohammadbrzbrz72 <mohammadbrzbrz72@gmail.com>
2025-09-17 21:52:23 +0330

    404.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import "./assets/styles/index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/pir_bate_mesal">
+    <HashRouter basename="/pir_bate_mesal">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
