67e8272c5dd4abee52192561fbbda4afe2b7e375
mohammadbrzbrz72 <mohammadbrzbrz72@gmail.com>
2025-09-22 14:03:53 +0330

    add writer page

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"; // Import the path module
-
-// import tsconfigPaths from "vite-tsconfig-paths";
-// import { reactRouter } from "@react-router/dev/vite";
+import path from "path";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig(() => {
-  // { command }
   const config = {
-    plugins: [
-      react(), // reactRouter(), tsconfigPaths()
-      tailwindcss(),
-      flowbiteReact(),
-    ],
+    plugins: [react(), tailwindcss(), flowbiteReact()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"), // '@' now represents the 'src' directory
         // Add more aliases as needed, e.g., '@components': path.resolve(__dirname, './src/components')
       },
     },
-    // It will be a our domain
     base: "/pir_bate_mesal",
-    //  build: {
-    //  rollupVersion: {
-    //      input: {
-    //        main: resolve(__dirname, "index.html"),
-    //        404: resolve(__dirname, "public/404.html"),
-    //      },
-    //  },
-    //  },
   };
 
-  //  if (command !== "serve") {
-  //    config.base = "/my-vite-react-app/";
-  //  }
   return config;
 });
