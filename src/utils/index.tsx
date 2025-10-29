dd59eac0ea6b376de0145cf687e71a600b7e2aa5
mohammadbrzbrz72 <mohammadbrzbrz72@gmail.com>
2025-10-29 19:09:08 +0330

    fix window bug for deploy

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -6,6 +6,8 @@ export function isExisted(data: strOrStrArr | undefined) {
 
 export function getNUMfromPathname() {
   // sample: http://localhost:5173/pir_bate_mesal/1501-2000-proverb/1721-1740
-  // return Number(window.location.pathname.split("/")[3].split("-")[0]);
-  return Number(window.location.pathname.split("/")[2].split("-")[0]);
+  return typeof window !== `undefined`
+    ? Number(window.location.pathname.split("/")[3].split("-")[0])
+    : null;
+  // return Number(window.location.pathname.split("/")[2].split("-")[0]);
 }
