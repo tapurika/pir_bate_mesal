bfabea34bb361decbd87b777b0ed740136e04b69
mohammadbrzbrz72 <mohammadbrzbrz72@gmail.com>
2026-02-10 16:03:54 +0330

    online nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-
-  reactCompiler: true,
+  // reactCompiler: true,
   // reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/pir_bate_mesal/" : "",
+  basePath: isProd ? "/pir_bate_mesal" : "",
 };
 
 export default nextConfig;
