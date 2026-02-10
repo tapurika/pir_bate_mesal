9ecd0642608561b35147b0b00fea0a57177f2159
mohammadbrzbrz72 <mohammadbrzbrz72@gmail.com>
2026-02-10 16:23:19 +0330

    online nextjs

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,10 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // reactCompiler: true,
-  // reactStrictMode: true,
+  reactCompiler: true,
+  reactStrictMode: true,
   output: "export",
+  // distDir: 'dist',
   images: {
     unoptimized: true, // Disable default image optimization
   },
