9276c3a31d737680a845b8e966e22bd6ad1cfb57
mohammadbrzbrz72 <mohammadbrzbrz72@gmail.com>
2026-02-13 13:07:09 +0330

    add new proverb

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,23 +1,15 @@
-import * as React from "react";
-
 import Header from "./Header";
 import Footer from "./Footer";
 
 const styles = {
   root: `
-     pt-3
+    pt-3
   `,
 };
 
-// const Layout: React.FC<PageProps & { pageTitle?: string }> = ({
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div
-      className={styles.root}
-      // style={{
-      //   direction: "rtl",
-      // }}
-    >
+    <div className={styles.root}>
       <Header />
       <main>{children}</main>
       <Footer />
